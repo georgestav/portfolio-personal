8ca3632aec719c4aa9de76b9d8e26d8005d4c7c6
George Stavroulakis <g.stavroulakis@gmail.com>
2022-04-06 14:01:43 +0200

    clear changes, prep for heroku

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,17 @@ import "./App.scss";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Section from "./components/Section";
+import { useState, useEffect } from "react";
 
 function App() {
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		fetch("/api")
+			.then((res) => res.json())
+			.then((data) => setData(data.message));
+	}, []);
+	console.log("api", data);
 	return (
 		<Fragment>
 			<Header />
