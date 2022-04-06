977002807b4e0fdffff3b4ee4e760c517bddb845
George Stavroulakis <g.stavroulakis@gmail.com>
2022-04-06 17:36:22 +0200

    add responsive nav menu

--- client/src/components/navigation/Navigation.tsx
+++ client/src/components/navigation/Navigation.tsx
@@ -1,25 +1,40 @@
 import "./Navigation.scss";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 function Navigation() {
-	return (
-		<nav className="header__links__container">
-			<a className="nav__items" href="#home">
-				HOME
-			</a>
-			<a className="nav__items" href="#skills">
-				SKILLS
-			</a>
-			<a className="nav__items" href="#portfolio">
-				PORTFOLIO
-			</a>
-			<a className="nav__items" href="#about-me">
-				ABOUT ME
-			</a>
-			<a className="nav__items" href="#contact-me">
-				CONTACT
-			</a>
-		</nav>
-	);
+    return (
+        <Navbar
+            collapseOnSelect
+            expand="lg"
+            bg="dark"
+            fixed="top"
+            variant="dark"
+            className="header__links__container"
+        >
+            <Container fluid>
+                <Navbar.Brand href="#home">
+                    <span className="logo">&gt;</span>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#skills" className="nav__items">
+                            SKILLS
+                        </Nav.Link>
+                        <Nav.Link href="#portfolio" className="nav__items">
+                            PORTFOLIO
+                        </Nav.Link>
+                        <Nav.Link href="#about-me" className="nav__items">
+                            ABOUT ME
+                        </Nav.Link>
+                        <Nav.Link href="#contact-me" className="nav__items">
+                            CONTACT
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default Navigation;
