7b0ffb323713158272ef25f079f0ab9d6e866402
George Stavroulakis <g.stavroulakis@gmail.com>
2022-04-09 15:07:09 +0200

    add contact me form, basic function

--- client/src/components/section/ContactMe.tsx
+++ client/src/components/section/ContactMe.tsx
@@ -1,38 +1,71 @@
 import "./ContactMe.scss";
+import { useRef } from "react";
 
 function ContactMe() {
+  const form = useRef(null);
+  // {
+  //   userFname: null,
+  //   userLname: null,
+  //   userEmail: null,
+  //   userPhone: null,
+  //   userMessage: null,
+  // }
+
+  const submitHandler = (e: any) => {
+    e.preventDefault();
+    //form?.current["userFname"].value
+    const mailto: string = `mailto:g.stavroulakis@gmail.com?subject=Interested in your work subject&body=My message`;
+    window.location.href = mailto;
+  };
+
   return (
     <div id="contact-me" className="contact__section">
       <h2 className="contact__title">CONTACT ME</h2>
-      <form action="" method="get" className="contact__form">
-        <div className="contact__form__row">
+      <form
+        className="contact__form"
+        ref={form}
+        onSubmit={(e) => submitHandler(e)}
+      >
+        {/* <div className="contact__form__row">
           <input
             type="text"
-            name="firstname"
-            id="fname"
-            placeholder="First Name *"
+            name="userFname"
+            placeholder="First Name"
+            required
           />
           <input
             type="text"
-            name="firstname"
-            id="lname"
-            placeholder="Last Name *"
+            name="userLname"
+            placeholder="Last Name"
+            required
           />
         </div>
         <div className="contact__form__row">
           <input
             type="email"
-            name="email"
-            id="email"
-            placeholder="Your Email *"
+            name="userEmail"
+            placeholder="Your Email"
+            required
           />
-          <input type="tel" name="firstname" id="phone" placeholder="Phone" />
+          <input
+            type="tel"
+            name="usePhone"
+            id="phone"
+            placeholder="Phone"
+            required
+          />
         </div>
         <div className="form__textarea">
           <label htmlFor="subject">My Message *</label>
-          <textarea name="" id="subject" cols={30} rows={10}></textarea>
-        </div>
-        <button className="form__submit" type="submit" value="sent" disabled>
+          <textarea
+            name="userMessage"
+            id="subject"
+            cols={30}
+            rows={10}
+            required
+          ></textarea>
+        </div> */}
+        <button className="form__submit" type="submit" value="sent">
           Send me a message
         </button>
       </form>
